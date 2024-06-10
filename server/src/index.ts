74cffd36f8a2ced6765861276bb9958e838bd488
Varenya7 <varenya.saini2021@vitstudent.ac.in>
2024-06-10 17:01:59 +0530

    initial commit

---
+++ server/src/index.ts
@@ -0,0 +1,25 @@
+// pwd : ax0WwFAdn6D1IU4n
+import express, { Express } from "express";
+import mongoose from "mongoose";
+import financialRecordRouter from "./routes/financial-records";
+
+import cors from "cors";
+
+const app: Express = express();
+const port = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(cors());
+
+const mongoURI: string =
+  "mongodb+srv://svv36912:ax0WwFAdn6D1IU4n@pfinance.hlwylle.mongodb.net/";
+
+  mongoose
+  .connect(mongoURI)
+  .then(() => console.log("CONNECTED TO MONGODB!"))
+  .catch((err) => console.error("Failed to Connect to MongoDB:", err));
+
+  app.use("/financial-records", financialRecordRouter);
+  app.listen(port, () => {
+    console.log(`Server Running on Port ${port}`);
+  });
